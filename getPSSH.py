2e98fc836b6d14b14884d943fd204de2edbc8546
Mikhail <medmm@inbox.ru>
2021-12-11 04:30:11 +0300

    minor fix

--- getPSSH.py
+++ getPSSH.py
@@ -1,21 +1,40 @@
 import requests, xmltodict, json
 
 def get_pssh(mpd_url):
+    pssh = ''
     r = requests.get(url=mpd_url)
     r.raise_for_status()
     xml = xmltodict.parse(r.text)
     mpd = json.loads(json.dumps(xml))
-    tracks = mpd['MPD']['Period']['AdaptationSet']
-    for video_tracks in tracks:
-        if video_tracks['@mimeType'] == 'video/mp4':
-            try:
-                for t in video_tracks['ContentProtection']:
-                    if t['@schemeIdUri'].lower() == "urn:uuid:edef8ba9-79d6-4ace-a3c8-27dcd51d21ed":
-                        pssh = t["cenc:pssh"]
-            except KeyError:
-                for t in video_tracks['Representation'][0]['ContentProtection']:
-                    if t['@schemeIdUri'].lower() == "urn:uuid:edef8ba9-79d6-4ace-a3c8-27dcd51d21ed":
-                        pssh = t["cenc:pssh"]['#text']
-            except TypeError:
-                pssh = input('Unable to find PSSH in mpd. Edit getPSSH.py or enter PSSH manually: ')        
+    periods = mpd['MPD']['Period']
+    if isinstance(periods, list):
+        for idx, period in enumerate(periods):
+            if isinstance(period['AdaptationSet'], list):
+                for ad_set in period['AdaptationSet']:
+                    if ad_set['@mimeType'] == 'video/mp4':
+                        try:
+                            for t in ad_set['ContentProtection']:
+                                if t['@schemeIdUri'].lower() == "urn:uuid:edef8ba9-79d6-4ace-a3c8-27dcd51d21ed":
+                                    pssh = t["cenc:pssh"]
+                        except KeyError:
+                            pass   
+            else:
+                if period['AdaptationSet']['@mimeType'] == 'video/mp4':
+                        try:
+                            for t in period['AdaptationSet']['ContentProtection']:
+                                if t['@schemeIdUri'].lower() == "urn:uuid:edef8ba9-79d6-4ace-a3c8-27dcd51d21ed":
+                                    pssh = t["cenc:pssh"]
+                        except KeyError:
+                            pass   
+    else:
+        for ad_set in periods['AdaptationSet']:
+                if ad_set['@mimeType'] == 'video/mp4':
+                    try:
+                        for t in ad_set['ContentProtection']:
+                            if t['@schemeIdUri'].lower() == "urn:uuid:edef8ba9-79d6-4ace-a3c8-27dcd51d21ed":
+                                pssh = t["cenc:pssh"]
+                    except KeyError:
+                        pass   
+    if pssh == '':
+        pssh = input('Unable to find PSSH in mpd. Edit getPSSH.py or enter PSSH manually: ')
     return pssh
--- l3.py
+++ l3.py
@@ -13,16 +13,18 @@ from base64 import b64encode
 from getPSSH import get_pssh
 from wvdecryptcustom import WvDecrypt
 from cdm.formats import wv_proto2_pb2 as wv_proto2
+from urllib.parse import urlparse
 import logging
 # logging.basicConfig(level=logging.DEBUG)
-
 MDP_URL = input('\nInput MPD URL: ')
 lic_url = input('License URL: ')
 # hardcoded for kinopoisk.ru
 # lic_url = 'https://widevine-proxy.ott.yandex.ru/proxy'
-
+responses = []
 pssh = get_pssh(MDP_URL)
-
+params = None
+params = urlparse(lic_url).params
+# pssh = 'AAAAXHBzc2gAAAAA7e+LqXnWSs6jyCfc1R0h7QAAADwIARIQ7iYSc3cNGm7XKPe3hSn3MhoIdXNwLWNlbmMiGDdpWVNjM2NOR203WEtQZTNoU24zTWc9PSoAMgA='
 # params from mdp_url:
 # ottsession=5945048d6f844d1699054cc5d44548f1&
 # puid=339572866&
@@ -34,21 +36,19 @@ def WV_Function(pssh, lic_url, cert_b64=None):
 	"""main func, emulates license request and then decrypt obtained license
 	fileds that changes every new request is signature, expirationTimestamp, watchSessionId, puid, and rawLicenseRequestBase64 """
 	wvdecrypt = WvDecrypt(init_data_b64=pssh, cert_data_b64=cert_b64, device=deviceconfig.device_android_generic)                   
-	response = requests.post(url=lic_url, headers=headers.headers, data=wvdecrypt.get_challenge())
-	if response.status_code == 200:
-		widevine_license = response
-	elif response.status_code != 200:
-		request = b64encode(wvdecrypt.get_challenge())
-		response = requests.post(url=lic_url, headers=headers.headers,
+	request = b64encode(wvdecrypt.get_challenge())
+	signature = cdm.hash_object
+	responses.append(requests.post(url=lic_url, headers=headers.headers, data=wvdecrypt.get_challenge(), params=params))
+
+	responses.append(requests.post(url=lic_url, headers=headers.headers, params=params, 
 		json={
 		"rawLicenseRequestBase64": str(request, "utf-8" ), 
-		})
-		if response.status_code == 200:
-			widevine_license = response
-		else:
-			request = b64encode(wvdecrypt.get_challenge())
-			signature = cdm.hash_object
-			widevine_license = requests.post(url=lic_url, headers=headers.headers,
+		}))
+	responses.append(requests.post(url=lic_url, headers=headers.headers, params=params, 
+		json={
+		"rawLicenseRequestBase64": str(request, "utf-8" ), 
+		}))
+	responses.append(requests.post(url=lic_url, headers=headers.headers, params=params, 
 			json={
 			"rawLicenseRequestBase64": str(request, "utf-8" ), 
 			"puid": 				'339572866',
@@ -63,12 +63,21 @@ def WV_Function(pssh, lic_url, cert_b64=None):
 			"signature": 			str(signature), 
 			# "signature":'b6ca3161c8bd38105e87770458aee16191214cfa', That is fucking amazon aws signing protocol!! V4!!
 			"version":				'V4'
-			})	
-	
-	print(f'{chr(10)}license response status: {widevine_license}{chr(10)}')
-	if widevine_license.status_code != 200:
-		print(f'server did not issue license, check json params in POST request.{chr(10)}')
+			}))
 
+	for idx, response in enumerate(responses):
+		try:
+			str(response.content, "utf-8")
+		except UnicodeDecodeError:
+			widevine_license = response
+			print(f'{chr(10)}license response status: {widevine_license}{chr(10)}')
+			break	
+		if idx == len(responses) - 1:
+			widevine_license = response
+			print(f'{chr(10)}license response status: {widevine_license}')
+			print(f'server reports: {str(widevine_license.content, "utf-8")}')
+			print(f'server did not issue license, check json params in POST request.{chr(10)}')	
+	
 	try: 
 		license_b64 = b64encode(widevine_license.content)
 	except TypeError:
@@ -78,6 +87,7 @@ def WV_Function(pssh, lic_url, cert_b64=None):
 	Correct, keyswvdecrypt = wvdecrypt.start_process()
 	if Correct:
 		return Correct, keyswvdecrypt   
+
 correct, keys = WV_Function(pssh, lic_url)
 
 for key in keys:
